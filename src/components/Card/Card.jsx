62a21767e2f3d418252b3cf7b48f2af2ed63768f
Jean Paul  Inofuente Cruz <kiral626@gmail.com>
2024-04-22 10:26:07 -0500

    Fixed Rating

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,10 +8,9 @@ const Card = (props) => {
           src={props.photo}
           className="rounded-3xl lg:w-[395px] lg:h-[269px] md:w-[450px] md:h-[300px] object-cover"
           alt={props.title}
-          loading="lazy"
         />
       </div>
-      <div className="flex mt-4 items-center gap-3 sm:mb-1 mb-2">
+      <div className="flex mt-4 items-center gap-3 sm:mb-2 mb-2">
         {props.superHost && (
           <p className="uppercase border text-xs border-gray3 rounded-2xl px-3 py-[0.45rem] text-gray3 font-bold">
             Super Host
@@ -19,7 +18,7 @@ const Card = (props) => {
         )}
 
         <p className="font-medium text-gray4 text-sm">{props.type}</p>
-        <div className="flex gap-1 ms-auto">
+        <div className="flex gap-1 ms-auto px-4">
           <span className="material-symbols-rounded text-darkOrange">star</span>
           <p className="mt-[0.12rem]">{props.rating}</p>
         </div>
